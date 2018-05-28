c1f4977960e48f7037813c2413f6117cd7e4bf38
Konrad <sabaku92@gmail.com>
2018-05-28 21:51:06 +0200

    Initial commit

---
+++ InterThings_b/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class InterthingsBConfig(AppConfig):
+    name = 'InterThings_b'
---
+++ InterThings_f/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class InterthingsFConfig(AppConfig):
+    name = 'InterThings_f'
